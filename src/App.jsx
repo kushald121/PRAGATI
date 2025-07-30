bfa220698803a2dec6ea405e64fa691384746f48
unknown <mohdsshadab552004@gmail.com>
2025-07-30 19:23:59 +0530

    page/contactPage shadab group-8

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "./components/navbar";
 import Profileicon from "./components/profileicon";
+import ContactPage from './pages/ContactPage';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Profileicon/>
+      <ContactPage />
     </div>
   )
 }
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Contact from '../components/Contact'
+import Faq from '../components/Faq'
+import Chatbot from '../components/Chatbot'
+
+
+const ContactPage = () => {
+  return (
+    <div className=''>
+        <Contact />
+        <Faq />
+        <Chatbot />
+    </div>
+  )
+}
+
+export default ContactPage
